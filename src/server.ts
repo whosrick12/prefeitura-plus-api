f01b17b6b6a7f593fd4178d86a07bfe3113d32c4
Ricardo S Almeida <ricardosoaress0811@gmail.com>
2026-06-24 21:51:07 -0300

    Fix: porta 8080

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,7 @@ console.log('JWT_KEY valor:', process.env.JWT_KEY);
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
